9a32e4ab8b51cb6134d14c2ce10e4dd52c7c505e
bitbytebit1 <tribe321@gmail.com>
2021-09-29 22:11:59 +0200

    1 file modified

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,9 @@
         These components stagger2 just fine {{ item }}
       </BaseCard>
     </TransitionGroupStaggered>
+    <button @click="$router.push({ name: 'theOtherpage' })">
+      Go to the the other page
+    </button>
   </div>
 </template>
 
